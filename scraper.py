5efe74211b72d0bedd97690013f3850126f4b3ed
sashaqd <106297572+sashaqd@users.noreply.github.com>
2023-03-31 13:01:09 +0400

    updated metadata script

--- scraper.py
+++ scraper.py
@@ -6,7 +6,11 @@ from selenium.webdriver.chrome.options import Options
 
 import os
 import time
-page = 1
+import csv
+
+pageCount = 1 #page to start from
+nextPage = 16 #next button
+pageHelper = 1
 
 # Path to the folder where downloaded data sets will be saved
 save_folder = '/Users/sasha/desktop/bayanat_data'
@@ -24,27 +28,76 @@ wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".kt-pagination__link--a
 
 links = []
 names = []
+
+with open('/Users/sasha/desktop/bayanat_data/metadata', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    header = ['name','description','tags']
+    writer.writerow(header)
+
+
+
 while True:
     # Scrape data from the current page
     div_element = driver.find_element(By.CSS_SELECTOR,"#datasetListing > div")
     div_children = div_element.find_elements(By.XPATH,"./*")
 
     for child in div_children:
+
+        csv_data = []
+
         desired_child = child.find_element(By.XPATH,".//*[contains(@class, 'kt-widget4__username')][1]")
-        print(desired_child.get_attribute("innerHTML"))
+        print(desired_child.get_attribute("innerHTML").strip())
         names.append(desired_child.get_attribute("innerHTML"))
+        curName = desired_child.get_attribute("innerHTML").strip()
+        csv_data.append(curName)
         link = desired_child.get_attribute("href")
         links.append(link)
         links.append("/")
 
-    print(page)
-    page += 1
-    if page == 4:
+        try:
+            description_div = child.find_element(By.XPATH,".//*[contains(@class, 'kt-widget4__text')][1]")
+            #print(description_div.get_attribute("innerHTML").strip())
+            description = description_div.get_attribute("innerHTML").strip()
+        except:
+            description = "NA"
+        csv_data.append(description)
+
+
+        try:
+            tag_div = child.find_element(By.XPATH,".//*[contains(@class, 'badges-container mt-2')][1]")
+            tags = tag_div.find_elements(By.XPATH,"./*")
+            tagList = []
+
+            for t in tags:
+                #print(t.get_attribute("innerHTML"))
+                tagList.append(t.get_attribute("innerHTML"))
+                csv_data.append(t.get_attribute("innerHTML"))
+        except:
+             csv_data.append("NA")
+
+        with open('/Users/sasha/desktop/bayanat_data/metadata', 'a', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(csv_data)
+
+    print(pageCount)
+    if pageCount == 261:
         break
+    pageCount += 1
+
 
     try:
         #Find the next page link and click it to load the next page
-        selector_path = "#page-selection > ul > li:nth-child(" + str(page + 3) + ")"
+        if pageCount == nextPage:
+            pageHelper = 1
+            selector_path = "#page-selection > ul > li:nth-child(" + str(pageHelper + 3) + ")"
+            pageHelper += 1
+            nextPage += 14
+        else:
+            selector_path = "#page-selection > ul > li:nth-child(" + str(pageHelper + 3) + ")"
+            pageHelper += 1
+
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,  selector_path)))
         element = driver.find_element(By.CSS_SELECTOR, selector_path)
         element.click()
         time.sleep(5)
